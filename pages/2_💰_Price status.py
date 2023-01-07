d5bd9709d369bb5fcf63085f17c96b7718e826bc
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-07 11:06:59 +0330

    Update 2_💰_Price status.py

--- pages/2_💰_Price status.py
+++ pages/2_💰_Price status.py
@@ -70,13 +70,13 @@ with subtab_ALICE:
                   
 
 with subtab_AXS:
-c1, c2 = st.columns(2)
-with c1:
+  c1, c2 = st.columns(2)
+  with c1:
         df = ALICE_Price
         fig = px.bar(df, x='DATE', y='RoPC', title='Range of Price Changes', log_y=False)
         fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD')
         st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-with c2:
+  with c2:
         df = ALICE_Price_Metric
         fig = px.line(df, x='Day', y='Price', color='TYPE', title='Price per Day', log_y=False)
         fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD', xaxis={'categoryorder':'total ascending'})
