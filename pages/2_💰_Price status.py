890dbe799fc40ce7c5b5c1a78be461c6a3648fa7
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-09 02:10:27 +0330

    Update 2_💰_Price status.py

--- pages/2_💰_Price status.py
+++ pages/2_💰_Price status.py
@@ -64,17 +64,19 @@ ALICE_Price_ATH = get_data('ALICE Price ATH')
 
 subtab_ALICE, subtab_AXS, subtab_ENJ, subtab_MANA, subtab_SAND = st.tabs(['ALICE', 'AXS', 'ENJ', 'MANA','SAND'])
 with subtab_ALICE:
-     df = ALICE_Price_ATH
-     st.metric(label='ALICE Price ATH', value=df['Price ATH'])
+     
      c1, c2 = st.columns(2)
      with c1:
+	     df = ALICE_Price_ATH
+             st.metric(label='ALICE Price ATH', value=df['Price ATH'])
              df = ALICE_Price
              fig = px.bar(df, x='DATE', y='RoPC', title='Range of Price Changes', log_y=False)
              fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD')
              st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
-     df = ALICE_Price_ATH
-     st.metric(label='Range of Price Change', value=df['RoPC'])	     
+     	     
      with c2:
+	    df = ALICE_Price_ATH
+            st.metric(label='Range of Price Change', value=df['RoPC'])
             df = ALICE_Price_Metric
             fig = px.line(df, x='Day', y='Price', color='TYPE', title='Price per Day', log_y=False)
             fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='$USD', xaxis={'categoryorder':'total ascending'})
