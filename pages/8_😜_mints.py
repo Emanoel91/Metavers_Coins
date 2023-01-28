2830572ac59fe0531dae38e202c789041031a531
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-28 16:35:48 +0330

    Update 8_😜_mints.py

--- pages/8_😜_mints.py
+++ pages/8_😜_mints.py
@@ -19,7 +19,6 @@ import time
 
 import numpy as np
 import pandas as pd
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
