fd5d3eee600029a599805288b045001362e75ae1
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-28 15:48:23 +0330

    Update 4_🚛_test.py

--- pages/4_🚛_test.py
+++ pages/4_🚛_test.py
@@ -31,13 +31,6 @@ st.write(data)
 
 st.header("Matplotlib chart in Plotly")
 
-import matplotlib.pyplot as plt
-
-f = plt.figure()
-arr = np.random.normal(1, 1, size=100)
-plt.hist(arr, bins=20)
-
-st.plotly_chart(f)
 
 
 ###
